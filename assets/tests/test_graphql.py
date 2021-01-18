20c355d19594ccd432de1ce2455d6a59da222497
rkorytov <rkorytoff@gmail.com>
2021-01-18 23:47:45 +0300

    some test for graphql

--- assets/tests/test_graphql.py
+++ assets/tests/test_graphql.py
@@ -1,15 +1,28 @@
 import json
-
+from django.urls import reverse
 from graphene_django.utils.testing import GraphQLTestCase
+from assets.models import *
+from graphene_django.utils.testing import graphql_query
+from django.contrib.auth.models import User
 
-class AssetTestCase(GraphQLTestCase):
 
-    def test_some_query(self):
+class AssetQueryTest(GraphQLTestCase):
+    def setUp(self):
+        self.username = 'john'
+        self.password = 'secret123'
+        self.email = 'john@doe.com'
+        self.url = reverse('assets')
+        self.user = User.objects.create_user(username=self.username, email=self.email, password=self.password)
+        self.client.login(username=self.username, password=self.password)
+        self.account = Account.objects.create(user=self.user, name='testAccount', amount=100000)
+        self.template = Template.objects.create(name='testTemplate', description='', url='test.com', key='template')
+
+    def test_get_template_by_key_query(self):
         response = self.query(
             '''
             query($key: String!) {
                 getTemplateByKey(key:$key) {
-                    id
+                    name
                 }
             }
             ''',
@@ -17,6 +30,20 @@ class AssetTestCase(GraphQLTestCase):
         )
 
         content = json.loads(response.content)
-
-        # This validates the status code and if you get errors
         self.assertResponseNoErrors(response)
+        self.assertEqual('testTemplate', content['data']['getTemplateByKey'][0]['name'])
+
+    def test_user_accounts_query(self):
+        response = graphql_query(
+            '''
+            query{
+              userAccounts{
+                name
+              }
+            }
+            ''',
+            client=self.client
+        )
+        content = json.loads(response.content)
+        self.assertResponseNoErrors(response)
+        self.assertEqual('testAccount', content['data']['userAccounts'][0]['name'])
---
+++ assets/tests/test_models.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from assets.models import *
+
+class AssetModelTest(TestCase):
+    fixtures = []
+
+    def test_account(self):
+        # account = Account.objects.first()
+        # self.assertEqual('test', account.name)
+        pass
