af7d489f093b98f3d1ce23ba696ee9c8eaed2a6a
rkorytov <rkorytoff@gmail.com>
2021-04-04 02:46:28 +0300

    terroform remote state

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,40 @@ provider "aws" {
   region = var.region
 }
 
+//resource "aws_s3_bucket" "terraform_state" {
+//  bucket = "terraform-baa-system"
+//  versioning {
+//    enabled = true
+//  }
+//  server_side_encryption_configuration {
+//    rule {
+//      apply_server_side_encryption_by_default {
+//        sse_algorithm = "AES256"
+//      }
+//    }
+//  }
+//}
+//
+//resource "aws_dynamodb_table" "terraform_locks" {
+//  name         = "terraform-up-and-running-locks"
+//  billing_mode = "PAY_PER_REQUEST"
+//  hash_key     = "LockID"
+//  attribute {
+//    name = "LockID"
+//    type = "S"
+//  }
+//}
+//
+//terraform {
+//  backend "s3" {
+//    bucket         = "terraform-up-and-running-state"
+//    key            = "global/s3/terraform.tfstate"
+//    region         = "us-east-1"
+//    dynamodb_table = "terraform-up-and-running-locks"
+//    encrypt        = true
+//  }
+//}
+
 resource "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda"
 
