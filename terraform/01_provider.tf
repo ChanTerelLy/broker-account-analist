8fc01f1eb6b97090ae51c14a8abf239ca8560b25
rkorytov <rkorytoff@gmail.com>
2021-06-06 21:36:41 +0300

    terraform save remote state

--- terraform/01_provider.tf
+++ terraform/01_provider.tf
@@ -1,3 +1,37 @@
 provider "aws" {
   region = var.region
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-baa-sync"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-west-1"
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-${var.project_name}-sync"
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-up-and-running-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
