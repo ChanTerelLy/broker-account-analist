f4d9a2b27473f5c9458a0509b401ba9aea7f1349
rkorytov <rkorytoff@gmail.com>
2021-11-07 22:26:36 +0300

    remove front, add ec2 ip output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,13 +2,13 @@ output "aws_ecs_task_definition" {
   value = aws_ecs_task_definition.app.arn
 }
 
-#data "aws_instances" "prod" {
-#  instance_tags        = {
-#    "Service" : "BAA",
-#    "Environment" : "Production"
-#  }
-#  instance_state_names = ["running", "stopped"]
-#}
-#output ec2-ids {
-#    value = data.aws_instances.prod.id
-#}
+data "aws_instances" "prod" {
+  instance_tags        = {
+    "Service" : "BAA",
+    "Stage" : "Prod"
+  }
+  instance_state_names = ["running", "stopped"]
+}
+output ec2-ids {
+    value = data.aws_instances.prod[*].public_ips
+}
