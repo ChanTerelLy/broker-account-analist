956f1d84e09df225924bbb5642031df35a7e2009
rkorytov <rkorytoff@gmail.com>
2021-11-10 20:04:37 +0300

    add efs

---
+++ terraform/13_efs.tf
@@ -0,0 +1,17 @@
+module "efs" {
+  source  = "cloudposse/efs/aws"
+  version = "0.32.0"
+
+  name                      = "baa-storage"
+  efs_backup_policy_enabled = false
+  region                    = var.region
+  vpc_id                    = module.vpc.vpc_id
+  subnets                   = module.vpc.private_subnets
+  transition_to_ia          = ["AFTER_90_DAYS"]
+
+  access_points = {
+    "baa_mount": "/"
+  }
+
+  allowed_security_group_ids = [module.efs_sg.security_group_id]
+}
